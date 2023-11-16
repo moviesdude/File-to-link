fd38fa16934e143f1b6132d73b1eb35ba4f2e205
MD Bots <133237308+moviesdude@users.noreply.github.com>
2023-11-16 19:42:23 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -13,12 +13,12 @@ class Var(object):
     API_HASH = str(getenv('API_HASH', 'd973fa8af375787c85dd6d2dfac94d7e'))
     BOT_TOKEN = str(getenv('BOT_TOKEN', '6488689300:AAH6TCrcskFOMi2rFtD6nHQPK4Ninog9qUM'))
     name = str(getenv('name', 'Fast_Downloader_bot'))
-    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '30'))
+    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '0'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002008584034'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", ""))  # 20 minutes
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "0"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5204439926").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
